b2973aa9d5c2cf8f88fc25ee21530c1d175b85fd
Tim Huang <taxihuang030926@gmail.com>
2025-09-02 21:22:10 +0800

    update

--- main.py
+++ main.py
@@ -13,4 +13,4 @@ async def health_check():
     return {"status": "OK"}
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="localhost", port=8000)
+    uvicorn.run(app, host="127.0.0.1", port=8001)
